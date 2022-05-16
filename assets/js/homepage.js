15b76a433c77496fd484e9e1253027a360a9d005
russe433 <russe433@msu.edu>
2022-05-15 22:06:13 -0400

    added stuff

---
+++ assets/js/homepage.js
@@ -0,0 +1,11 @@
+var getUserRepos = function () {
+    fetch("https://api.github.com/users/octocat/repos");
+};
+
+getUserRepos()
+
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+     });
+    });
